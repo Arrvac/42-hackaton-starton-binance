759f03ceb8d9cfb76e054f755b92abbb251ec3b2
tyuan <tyuan@student.42.fr>
2022-03-20 15:07:12 +0100

    feat: book

--- frontend/src/navigation/Routes.tsx
+++ frontend/src/navigation/Routes.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { CreateBooks } from "../pages/CreateBooks";
+import { Book } from "../pages/Book";
 import { Home } from "../pages/Home";
 
 export const Routes: FC = () => {
@@ -9,6 +10,7 @@ export const Routes: FC = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path={`/create-book`} component={CreateBooks} />
+        <Route exact path={`/book/:id`} component={Book} />
 
         <Redirect to="/" />
       </Switch>
